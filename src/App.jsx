6e5240329afd04579358f2b7fb9dc02de6b0f112
it-giants <a.e.i9444@gmail.com>
2025-01-23 23:22:39 +0300

    Header (done)

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,16 @@
+import Navbar from "./components/Navbar"
+
 const App = () => {
   return (
-    <div className=''>Hello World</div>
+    <div className='px-4 md:px-8 lg:px-16 xl:pd-32 xxl:pd-64'>
+      {/* NAVBAR */}
+      <Navbar />
+      
+      {/* BREADCRUMB */}
+      {/* INTRODUCTION */}
+      {/* FEATURED POSTS */}
+      {/* POST LIST */}
+    </div>
   )
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+function Navbar() {
+    const [open, setOpen] = useState(false);
+    return (
+        <div className="w-full h-16 md:h-20 flex items-center justify-between">
+            {/* LOGO */}
+            <div className="flex items-center gap-4">
+                <img src="/logo.png" className="w-8 h-8" alt="" />
+                <span className="font-bold">lamalog</span>
+            </div>
+
+            {/* MOBILE MENU */}
+            <div className="md:hidden cursor-pointer text-4xl" onClick={() => setOpen(prev => !prev)}>
+                {
+                    open ? "X" : "☰"
+                }
+            </div>
+
+            {/* MOBILE LINKS LIST */}
+            <div className={`w-full h-screen flex flex-col items-center justify-center bg-[#e6e6ff] gap-8  font-medium text-lg absolute top-16 transition-all ${open ? "right-0" : "-right-[100%]"}`}>
+                <a href="/">Home</a>
+                <a href="/">Trending</a>
+                <a href="/">Most Popular</a>
+                <a href="/">About</a>
+                <a href="/">
+                    <button className="py-2 px-4 rounded-3xl bg-blue-800 text-white">Login 👋🏻</button>
+                </a>
+            </div>
+
+            {/* DESKTOP MENU */}
+            <div className="hidden md:flex items-center gap-8 xl:gap-12 font-medium">
+                <a href="/">Home</a>
+                <a href="/">Trending</a>
+                <a href="/">Most Popular</a>
+                <a href="/">About</a>
+                <a href="/">
+                    <button className="py-2 px-4 rounded-3xl bg-blue-800 text-white">Login 👋🏻</button>
+                </a>
+            </div>
+        </div>
+    );
+}
+
+export default Navbar;
